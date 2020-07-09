581747e662e31182a44f27c1eefbd53d6541b22d
u-sayuri <splitpea215@gmail.com>
2020-07-09 17:53:22 +0700

    Added core features and updated .gitignore

--- sayumi.js
+++ sayumi.js
@@ -4,6 +4,7 @@ const path = require("path");
 
 const Discord = require("discord.js");
 const Winston = require("winston");
+const { error } = require("console");
 
 
 require("dotenv").config;
@@ -11,8 +12,9 @@ const {TOKEN, master} = process.env;
 
 const client = new Discord.Client();
 const cooldown = new Discord.Collection();
-client.commands = new Discord.Collection();
-client.aliases = new Discord.Collection();
+const MentionCooldown = new Discord.Collection();
+client.commandsList = new Discord.Collection();
+client.aliasesList = new Discord.Collection();
 client.cagetories = new Discord.Collection();
 
 //Date-time system
@@ -47,12 +49,14 @@ const logger = Winston.createLogger({
       new Winston.transports.Console(),
       new Winston.transports.File({ filename: "./log.txt", json: false }),
     ],
-    format: winston.format.printf(
+    format: Winston.format.printf(
       log => `[${log.level.toUpperCase()}] - ${log.message} (${days} - ${hrs}:${min}:${sec}) <GMT +7>`),
   });
-  client.on("ready", () => {
+
+client.on("ready", () => {
     logger.log("info", "Status 200");
   });
+  
   client.on("debug", m => logger.log("debug", m));
   client.on("warn", m => logger.log("warn", m));
   client.on("error", e => logger.log("error", e));
@@ -65,22 +69,6 @@ const logger = Winston.createLogger({
     console.error("Uncaught Promise Rejection:\n", error.name + ": " + error.message + `\n- Details:\t`);
     console.error(error);
   });
-
-//Local database init
-let database = [];
-fs.readdirSync(Root).forEach(file => {
-    if (file !== "db.txt") return;
-    const dbFile = "db.txt";
-    if (!dbFile) {
-    fs.writeFile("db.txt", (err) => {
-        console.log(err);
-        return;
-    });
-    } else if (dbFile) {
-    database = fs.readFile(dbFile);
-    return database;
-    }
-});
 
 //Status message and misc.
 client.once("ready", () => {
@@ -140,10 +128,10 @@ const CommandLoad = (dir) => {
         else if (file.endsWith(".js")) {
             FileCount++;
             const Command = require(dir);
-            if (!Command.Issue || !Command.Issue === {}) {
+            if (!Command.issue || !Command.issue === {}) {
                 EmptyCommandFileCount++;
             }
-            if (!Command.status === false) {
+            if (Command.status === false) {
                 UnexecutableFileCount++
             } 
             else ExecutableFileCount++;
@@ -155,36 +143,314 @@ const CommandLoad = (dir) => {
 
 CommandLoad(Commands);
 
-console.log(`[INFO] Successfully loaded ${FileCount} commands`);
-if (UnexecutableFileCount > 0) { 
-    console.log(`with ${UnexecutableFileCount} files disabled`);
-}
-if (EmptyCommandFileCount > 0) {
-    console.log(`with ${EmptyCommandFileCount} files empty`);
+if (FileCount <= 0) {
+    console.log("[INFO] The directory is currently empty!")
+} else {    
+    console.log(`[INFO] Successfully loaded ${FileCount} commands`);
+    if (UnexecutableFileCount > 0) { 
+        console.log(`with ${UnexecutableFileCount} files disabled`);
+    }
+    if (EmptyCommandFileCount > 0) {
+        console.log(`with ${EmptyCommandFileCount} files empty`);
+    }
 }
 
+// Local database init
+let database = [];
+FileSystem.readdirSync(Root).forEach(file => {
+    if (file !== "db.txt") return;
+    const dbFile = "db.txt";
+    if (!dbFile) {
+    FileSystem.writeFile("db.txt", (err) => {
+        console.log(err);
+        return;
+    });
+    } else if (dbFile) {
+    database = FileSystem.readFile(dbFile);
+    return database;
+    }
+});
+
+// Message events
 client.on("message", async message => {
     let Guild = JSON.parse("./GuildList.json", "utf8");
-    if (!Guild[message.guild.id]) {
-        Guild[message.guild.id] = {
-            prefix: process.env.defaultPrefix,
-        }
-    }
-    let prefix = Guild[message.guild.id].prefix;
-
     let Channel = JSON.parse("./ChannelStatus.json", "utf8");
-    if (!Channel[message.channel.id]) {
-        Channel[message.channel.id] = {
-            FalseCMDReply: process.env.defaultFalseCMDReply,
-            AllowReply: process.env.defaultReplyStatus
+
+    let prefix = process.env.defaultPrefix;
+    let FalseCMDReply = process.env.defaultFalseCMDReply;
+    let ReplyStatus = process.env.defaultReplyStatus;
+
+    if (message.guild) {
+        if (!Guild[message.guild.id]) {
+            Guild[message.guild.id] = {
+                prefix: process.env.defaultPrefix,
+            }
         }
+        prefix = Guild[message.guild.id].prefix;
+    }
+
+    if (message.channel && message.channel.type !== "dm") {
+        if (!Channel[message.channel.id]) {
+            Channel[message.channel.id] = {
+                FalseCMDReply: process.env.defaultFalseCMDReply,
+                AllowReply: process.env.defaultReplyStatus
+            }
+        }
+        FalseCMDReply = Channel[message.channel.id].FalseCMDReply;
+        ReplyStatus = Channel[message.channel.id].AllowReply;
     }
     
-    let FalseCMDReply = Channel[message.channel.id].FalseCMDReply;
-    let ReplyStatus = Channel[message.channel.id].AllowReply;
-    
-    if (!message.client.permissions.has("SEND_MESSAGES") || !message.client.permissions.has("VIEW_CHANNELS")) return;
+    if (message.guild) {
+    if (!message.client.permissions.has("SEND_MESSAGES") || !message.client.permissions.has("READ_MESSAGE_HISTORY")) return;
+    if (ReplyStatus === false) {
+        if (!message.member.hasPermission("ADMINISTRATOR")) return;
+    } 
 
+    }
     const args = message.content.slice(prefix.length).split(/ +/);
+    const CommandName = args.shift().toLowerCase();
+    const command = client.commandsList.get(CommandName) || 
+                    client.commandsList.find(cmd => cmd.aliases && cmd.aliases.includes(CommandName)
+                    );
+    const content = message.content.toLowerCase();
+
+    // if (content.includes(prefix, 0) && content.includes(CommandName, 1)) {}
+
+    // Mention respond (general)
+    let MentionedMassage = false;
+    if (!message.content.startsWith(prefix)) {
+        if (message.author.bot) return;
+        if (contents.startsWith(`<@${client.user.id}>`) && !message.author.bot) {
+            if (message.author.id === master) {
+                const reply = ["", "", "", "Not now.", "No."]
+                const respond = reply[Math.floor(Math.random() * reply.length)];
+                message.channel.send(respond);
+                MentionedMassage = true;
+            }
+            else { 
+                const reply = [
+                `My prefix is \`${prefix}\``,
+                `Type \`${prefix}help\` to see command index.`,
+                `Am I a human?`
+                ];
+                const respond = reply[Math.floor(Math.random() * reply.length)];
+                if (MentionedMassage === false) return message.channel.send(respond);
+                if (MentionedMassage === true) return;
+                MentionedMassage = true;
+                logger.log(
+                    message.author.tag + " tagged me.\n Contents: " + `"${message.content}"`
+                );
+            }
+        }
+
+        setTimeout(7000).then(MentionedMassage = false);
+    
+        if (!MentionCooldown.has("mention")) {
+        MentionCooldown.set("mention", new Discord.Collection());    
+        }  
+        const MentionTimestamp = MentionCooldown.get("mention");
+        const PausedTime = 5000;
+
+        if (MentionTimestamp.has(message.author.id)) {
+            const MentionExpirationTime = timestamps.get(message.author.id) + PausedTime;
+        
+            if (now < MentionExpirationTime && message.author.id !== process.env.master) {
+              const timeLeft = (MentionExpirationTime - now) / 1000;
+                if (ReplyStatus === true) return message.channel.send(
+                `Wait ${timeLeft.toFixed(0)} more second${
+                  timeLeft > 1 ? "s" : ""
+                }...`
+                ).then(message.delete(3000));
+            } else;
+          }
+        MentionTimestamp.set(message.author.id, now);
+        setTimeout(() => MentionTimestamp.delete(message.author.id), PausedTime);
+
+    }
+
+    console.log("<Terminal>", "A command has been executed.");
+
+    if (!command) {
+        const typo = message.content.slice(prefix.length);
+        const NotACmd = [
+            "This is not a vaild command for me.",
+            `Perhaps a typo, ${message.author}?\n\`"${typo}"\``,
+            "I can't issue this.",
+            `What is *${typo}*?`,
+            `If that is an unadded feature, consider typing \`${prefix}feedback ${typo}\` if you want this feature/command added to my collection.`,
+        ];
+        const respond1 = NotACmd[Math.floor(Math.random() * NotACmd.length)];
+        
+        console.log(
+            "<Terminal>", 
+            `Unknown command '${CommandName}'. command execution has been cancelled.`
+        );
+        if (FalseCMDReply === true && ReplyStatus === true) return message.channel.send(respond1);
+    }
+
+    console.log("command name: " + CommandName);
+    const cmdArgs = message.content.slice(prefix.length + commandName.length + 2).split(/ +/);
+    if (cmdArgs === "") {
+        if (command.description === "") {
+        console.log(
+            "Command to issue: " +
+            `/${commandName}: ` +
+            "<no description> " +
+            "(empty_string)"
+        );
+        } 
+        if (!command.args) {
+        if (command.description) {
+            console.log(
+            "Command to issue: " +
+                `/${commandName}: ` +
+                command.description
+            );  
+        } else {
+            console.log(
+                "Command to issue: " +
+                `/${commandName}: ` +
+                "<no description> "
+            );
+            }
+        } else {
+        console.log(
+            "Command to issue: " +
+            `/${commandName}: ` +
+            command.description +
+            "(empty_string)"
+        );
+        }
+    } else {
+        console.log("command to issue: " + `/${commandName}: ` + `'${cmdArgs}'`);
+    }
+    // const BlankCmd = [
+    //     "Hmmmmmm..."
+    // ];
+    // const respond2 = BlankCmd[Math.floor(Math.random() * BlankCmd.length)];
+    //     if (message.content === `${prefix}`) {
+    //         if (ReplyStatus === true) return message.channel.send(respond2).then(m => m.delete(4000));
+    //         else return;
+    //     }
+
+    // Command cooldowns
+    if (!cooldown.has(Command.name)) {
+        cooldown.set(Command.name, new Discord.Collection());
+        }
+    
+        const timestamps = cooldown.get(Command.name);
+        const cooldownAmount = (Command.cooldown || 3) * 1000;
+    
+        if (timestamps.has(message.author.id)) {
+            const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+        
+            if (now < expirationTime && message.author.id !== ownerID) {
+                const timeLeft = (expirationTime - now) / 1000;
+                return message.reply(
+                    `please wait ${timeLeft.toFixed(0)} more second${
+                    timeLeft > 1 ? "s" : ""
+                    } before reusing the '${Command.name}' command.`
+                );
+            }
+        }
+    
+    timestamps.set(message.author.id, now);
+    setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+    // Responses when executing guild commands inside DMs
+    if (command.guildOnly && message.channel.type !== "text") {
+        console.log(
+          "<Terminal>",
+          `User ${message.author} has sent a guild command in direct message.`
+        );
+        const NoDM = [
+          "Unfortunately, this command cannot be executed inside DMs.",
+          "Sorry, but i can't do this inside a direct-message chat.",
+          "Make sure that you typed this command inside a server.",
+        ];
+        const respond = NoDM[Math.floor(Math.random() * NoDM.length)];
+        return message.reply(respond);
+    }
+
+    // Master-only commands
+    if (command.ownerOnly && message.author.id !== master) {
+        message.channel.send(
+          `Sorry ${message.author}, but this command can be issued by my master only.`
+        );
+        return;
+      }
+    
+    // Empty args responses
+    if (command.args && !args.length) {
+        console.log("<Args>", "Starting arguments detection..."),
+        console.log(
+            "<Args#return>",
+            `User ${message.author} didn't give any arguments. command cancelled.`
+        );
+        let string = command.prompt;
+        if (string === false) {
+           message.channel.send('Terminal standing by.').then(m => m.delete(4000));
+           return;
+        }
+        if (string === undefined || string === null || string === "") {
+            const NoArgs = [
+            `There is no arguments.`,
+            `Hey ${message.author}, I need an argument to work with.`,
+            `You didn't provide any arguments, ${message.author}!`,
+            "Hmmm...I don't see any arguments here.",
+            ];
+            string = NoArgs[Math.floor(Math.random() * NoArgs.length)];
+            if (command.usage) {
+                string += `\nUsage: \`${prefix}${command.name} ${command.usage}\`.`;
+            } 
+            if (ReplyStatus === true) return message.channel.send(string).then(m => m.delete(8000));
+        }
+        if (command.usage) {
+            string += `\nUsage: \`${prefix}${command.name} ${command.usage}\`.`;
+        }
+        if (ReplyStatus === true) return message.reply(string).then(m => m.delete(6000));
+      }
+
+    // Execute commands and error responses
+    try {
+        console.log("[DEBUG] Running command: " + command.name);
+        command.issue(message, args, client);
+      } catch (error) {
+        console.error(error);
+        logger.log("error", error);
+        console.log("<Error>", "Found " + error);
+        const err = [
+          `The command you're trying to issue is unavailable or being not issued due to some errors. Please try again later.`,
+          `Oops, looks like I've encounter an error. Error is being reported, I'm trying to fix this as soon as possible.`,
+        ];
+        if (message.channel.type !== "dm" && message.channel.type !== "voice") {
+          const channel = client.channels.get('630334027081056287');
+          const embed = new Discord.RichEmbed()
+            .setColor('#ff0000')
+            .setTitle('An error has occured.')
+            .setDescription(`*At ${message.channel} of server "${message.guild.name}"*\n*Issued by* - ${message.author.tag}:\n"${message.content}"`)
+            .addField('Error status ------------------------------------------', `\`${error}\``)
+            .setTimestamp();
+            channel.send(embed);
+        } else {
+          const channel = client.channels.get('630334027081056287');
+          const embed = new Discord.RichEmbed()
+            .setColor('#ff0000')
+            .setTitle('An error has occured.')
+            .setDescription(`*Issued by* ${message.author}`)
+            .addField('Error status ------------------------------------------', `\`${error}\``)
+            .setTimestamp();
+            channel.send(embed);
+        }
+        const respondErr = err[Math.floor(Math.random() * err.length)];
+        if (message.channel.type === "dm") return message.channel.send(respondErr)
+        else if (message.channel.type !== "dm") {
+            message.channel.send(respondErr).then(message.delete(4500));
+        }
+        return;
+      }
+    
 });
-client.login(TOKEN).catch(console.error);
+
+// Login.
+client.login(TOKEN).catch(console.error)
